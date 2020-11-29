87cd7a68ea0e6f004fbcb0bfc6c4a4d53e2b820d
jfederico <jesus@123it.ca>
2020-11-29 04:58:07 +0000

    cleaned up code and added some fetched data to form

--- assets/js/wbg-admin-script.js
+++ assets/js/wbg-admin-script.js
@@ -17,66 +17,26 @@
         this.parentElement.style.display = 'none';
     });
 
-    $('input[name=post_title]').on('change', function() {
-        console.debug('change ' + new Date());
+    $('input[name=post_title]').on('input', function() {
+        console.debug('input ' + new Date());
         if (this.value.length > 1) {
             $(this).attr('list', 'seek_list');
             $(this).searchBooks();
         } else {
             $(this).removeAttr('list')
         }
-    // }).on('input', function () {
-    //     console.debug(this);
-    //     var val = this.value;
-    //     console.debug(this.value);
-    //     if ($('#seek_list option').filter(function() {
-    //         console.debug('datalist input');
-    //         console.debug(this.value.toUpperCase() === val.toUpperCase());
-    //     }).length) {
-    //         //send ajax request
-    //         console.debug(this.value);
-    //     }
     // }).on('blur change click dblclick error focus focusin focusout hover keydown keypress keyup load mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup resize scroll select submit', function(){
-    }).on('input select', function() {
-        console.debug(arguments[0]);
+    }).on('select', function() {
+        console.debug('select ' + new Date());
         var id = $("#seek_list option[value='" + this.value + "']").attr('data-id');
-        console.debug($('#seek_list'));
-        console.debug($('#seek_list').find(`[data-id='${id}']`));
-        var selected = $('#seek_list option#' + id);
+        // var selected = $('#seek_list option#' + id);
+        var selected = $('#seek_list').find(`[data-id='${id}']`);
         console.log(selected);
-        selected.searchBook();
+        $(selected).searchBook();
     });
-    // .on('focusout', function() {
-    //     console.debug('datalist click');
-    //     console.debug(this);
-    //     var selected = $('datalist#seek_list').val();
-    //     console.debug(selected);
-    //     $('#wbg_publisher').val(selected.attr('data-publisher'));
-    // });
-    // .on('keydown', function(e) {
-    //     console.debug('keydown ' + new Date());
-    //     var code = e.keyCode || e.which;
-    //     if (code != 8 && code != 46) { //Backspace or Delete
-    //         // Do nothing, it will be processed on keyup.
-    //         return;
-    //     }
-    //     if (this.value.length > 1) {
-    //         $(this).attr('list', 'seek_list');
-    //         $(this).searchBooks();
-    //     } else {
-    //         $(this).removeAttr('list')
-    //     }
-    // });
-
-    // $('datalist[name=seek_list]').on('click', function() {
-    //     console.debug('datalist click');
-    //     console.debug(this);
-    //     var selected = $(this).val();
-    //     console.debug(selected);
-    //     $('#wbg_publisher').val(selected.attr('data-publisher'));
-    // });
 
     $.fn.searchBooks = function() {
+        console.debug('searchBooks()');
         return this.each(function() {
             // Search books by title.
             var url = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.value);
@@ -86,12 +46,9 @@
                 console.debug(result.items);
                 var datalist = $('datalist#seek_list');
                 datalist.empty();
-                // console.debug(data.title);
                 result.items.forEach(function(item) {
                     if (item.volumeInfo.title.startsWith(data.title)) {
-                        // console.debug(item);
-                        // console.debug(item.volumeInfo.title);
-                        datalist.prepend("<option id='" + item.id + "' value='" + item.volumeInfo.title + "' data-id='" + item.id + "'>");
+                        datalist.prepend("<option value='" + item.volumeInfo.title + "' data-id='" + item.id + "'>");
                     }
                  });
             });
@@ -99,13 +56,21 @@
     };
 
     $.fn.searchBook = function() {
-        return this.each(function(){
-            // Search book by id using data-id.
-            var url = 'https://www.googleapis.com/books/v1/volumes/' + $(this).attr("data-id");
-            console.debug('searching book ' + url + ' ' + new Date());
-            $.getJSON(url, data, function(result) {
-                console.debug(result.items);
-            });
+        console.debug('searchBook()');
+        // Search book by id using data-id.
+        var url = 'https://www.googleapis.com/books/v1/volumes/' + this.data('id');
+        console.debug('searching book ' + url + ' ' + new Date());
+        $.getJSON(url, {}, function(result) {
+            console.debug(result);
+            var wbg_publisher = result.volumeInfo.publisher;
+            $('input[name=wbg_publisher]').val(wbg_publisher);
+            var wbg_published_on = result.volumeInfo.publishedDate;
+            $('input[name=wbg_published_on]').val(wbg_published_on);
+            $('input[name=wbg_isbn]').val(result.volumeInfo.industryIdentifiers[0].identifier);
+            $('input[name=wbg_pages]').val(result.volumeInfo.pageCount);
+            $('input[name=wbg_country]').val(result.saleInfo.country);
+            $('input[name=wbg_language]').val(result.volumeInfo.language);
+            $('input[name=wbg_download_link]').val(result.volumeInfo.infoLink);
         });
     };
 })(jQuery);
