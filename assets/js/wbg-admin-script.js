d76ba9adb6112e5bd8ceb302ef564887a3243c1e
jfederico <jesus@123it.ca>
2020-11-30 14:59:50 +0000

    validated some data coming from the json file

--- assets/js/wbg-admin-script.js
+++ assets/js/wbg-admin-script.js
@@ -47,10 +47,17 @@
                 var datalist = $('datalist#seek_list');
                 datalist.empty();
                 result.items.forEach(function(item) {
-                    if (item.volumeInfo.title.startsWith(data.title)) {
+                    //console.debug(item.volumeInfo.title + " : " + data.title);
+                    if (item.volumeInfo.title === data.title || item.volumeInfo.title.startsWith(data.title)) {
                         datalist.prepend("<option value='" + item.volumeInfo.title + "' data-id='" + item.id + "'>");
                     }
-                 });
+                });
+                $("datalist#seek_list option").each(function(){
+                    var sameOpt = $(this).parent().find("[value='" + this.value + "']:gt(0)");
+                    sameOpt.val(function(i, val){
+                        return val + '-' + (sameOpt.index(this) + 2);
+                    });
+                });
             });
         });
     };
@@ -62,15 +69,38 @@
         console.debug('searching book ' + url + ' ' + new Date());
         $.getJSON(url, {}, function(result) {
             console.debug(result);
+            var wbg_title = result.volumeInfo.title;
+            $('input[name=post_title]').val(wbg_title);
+            var wbg_description = '';
+            if (typeof result.volumeInfo.description !== 'undefined') {
+                wbg_description = result.volumeInfo.description;
+            } else if (typeof result.volumeInfo.subtitle !== 'undefined') {
+                wbg_description = result.volumeInfo.subtitle;
+            }
+            console.log(wbg_description);
+            $('textarea[name=content]').html(wbg_description);
+            console.log($('textarea[name=content]'));
+            var wbg_author = result.volumeInfo.authors.join(', ');
+            $('input[name=wbg_author]').val(wbg_author);
             var wbg_publisher = result.volumeInfo.publisher;
             $('input[name=wbg_publisher]').val(wbg_publisher);
             var wbg_published_on = result.volumeInfo.publishedDate;
             $('input[name=wbg_published_on]').val(wbg_published_on);
-            $('input[name=wbg_isbn]').val(result.volumeInfo.industryIdentifiers[0].identifier);
+            var wbg_isbn = '';
+            if (typeof result.volumeInfo.industryIdentifiers !== 'undefined' && typeof result.volumeInfo.industryIdentifiers[0].identifier !== 'undefined') {
+                wbg_isbn = result.volumeInfo.industryIdentifiers[0].identifier;
+            }
+            $('input[name=wbg_isbn]').val(wbg_isbn);
             $('input[name=wbg_pages]').val(result.volumeInfo.pageCount);
             $('input[name=wbg_country]').val(result.saleInfo.country);
             $('input[name=wbg_language]').val(result.volumeInfo.language);
             $('input[name=wbg_download_link]').val(result.volumeInfo.infoLink);
+            var fifu_input_url = '';
+            if (typeof result.volumeInfo.imageLinks !== 'undefined' && typeof result.volumeInfo.imageLinks.thumbnail !== 'undefined') {
+                fifu_input_url = result.volumeInfo.imageLinks.thumbnail;
+            }
+            $('input[name=fifu_input_url]').val(fifu_input_url);
         });
     };
+
 })(jQuery);
